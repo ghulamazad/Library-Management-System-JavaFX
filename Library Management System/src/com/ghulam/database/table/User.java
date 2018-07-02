4a4753623460e1bb4f4baf4a3e6c0f5afda05193
Md.Ghulam Azad Ansari <Md.GhulamAzadAnsariOfficial@gmail.com>
2018-07-02 11:54:04 +0530

    Create User Class

---
+++ Library Management System/src/com/ghulam/database/table/User.java
@@ -0,0 +1,23 @@
+package com.ghulam.database.table;
+
+public class User {
+        //variables
+        private String username;
+        private String password;
+        //method
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
